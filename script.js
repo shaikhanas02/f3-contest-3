f3065db4178aff288c4a811ae61298b241ed9b83
shaikhanas02 <122043549+shaikhanas02@users.noreply.github.com>
2023-03-25 15:19:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 let x = document.getElementById('map') ;
 let btn = document.getElementById('btn') ;
 let removeBtn = document.getElementById('remove-btn') ;
-let frame = document.getElementById('frame') ;
+let frame = document.getElementById('map') ;
 
-
-// let iframe = document.createElement('iframe');
-// var html = '<body>Foo</body>';
-// iframe.src = 'https://maps.google.com/maps?q=${position.coords.longitude},${position.coords.latitude}&z=15&output=embed';
-// document.body.appendChild(iframe);
-
-
-//iframedoc = frame.contentWindow;
 
 btn.addEventListener('click', getLocation) ;
+removeBtn.addEventListener('click', clear) ;
 
 function getLocation() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(showPosition);
-
     } else { 
       x.innerHTML = "Geolocation is not supported by this browser.";
     }
   }
   
-  function showPosition(position) {
+  function showPosition(position){
 localStorage.setItem("Lat:", position.coords.latitude) 
 localStorage.setItem("Long:", position.coords.longitude) 
+console.log(position.coords.latitude, position.coords.longitude);
+  frame.innerHTML = `
+   
+   <iframe
+  src="https://maps.google.com/maps?q=${position.coords.latitude},${position.coords.longitude}&z=15&output=embed"
+  width="360"
+  height="270"
+  frameborder="0"
+  style="border:0"
+></iframe>`
   }
-  
-  function changeIframe() {
-     //iframedoc.src = "https://maps.google.com/maps?q=72.8856,19.0748&z=15&output=embed";
-     console.log(iframedoc) ;
-   }
-changeIframe() ;
+
+  function clear(){
+    frame.innerHTML= "" ;
+  }
